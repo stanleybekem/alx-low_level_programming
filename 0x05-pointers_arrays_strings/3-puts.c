1a2771fa575192094a0a180e744f457846fc7e39
stanleybekem <stanley10mtemi@gmail.com>
2023-09-18 10:52:25 -0700

    pointers arrays and strings

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,14 @@
 #include "main.h"
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
  */
-int main(void)
+void _puts(char *str)
 {
-    char *str;
-
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    _puts(str);
-    return (0);
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+		_putchar('\n');
 }
 
